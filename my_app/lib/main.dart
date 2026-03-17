bafcf1c85f3b755c31481fd03aa9e7580907e185
prajakta2005 <prajakta23joshi@gmail.com>
2026-03-17 23:15:22 +0530

    basic working app gradle on phone

--- my_app/lib/main.dart
+++ my_app/lib/main.dart
@@ -1,8 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
-import 'package:provider/provider.dart';
-import 'package:firebase_core/firebase_core.dart';
-
+ 
 import 'utils/app_theme.dart';
 import 'screens/login_screen.dart';
 import 'screens/dashboard_screen.dart';
@@ -19,49 +17,43 @@ void main() async {
   ]);
 
   runApp(const SolarCounterApp());
-}
-
+} 
 
 class SolarCounterApp extends StatelessWidget {
   const SolarCounterApp({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return MultiProvider(
-      
-      providers: const [
-        
-      ],
-      child: MaterialApp(
-       
-        title: 'Solar Hardware Counter',
-        debugShowCheckedModeBanner: false,
+    
+    return MaterialApp(
+      title: 'Solar Hardware Counter',
+      debugShowCheckedModeBanner: false,
 
-        theme:      AppTheme.lightTheme,
-        darkTheme:  AppTheme.darkTheme,
-        themeMode:  ThemeMode.system,
+     
+      theme:     AppTheme.lightTheme,
+      darkTheme: AppTheme.darkTheme,
+      themeMode: ThemeMode.system,
 
+     
+      initialRoute: AppRoutes.login,
 
-        initialRoute: AppRoutes.login,
+      routes: {
+        AppRoutes.login:     (_) => const LoginScreen(),
+        AppRoutes.dashboard: (_) => const DashboardScreen(),
+        AppRoutes.counter:   (_) => const CounterScreen(),
+        AppRoutes.logForm:   (_) => const LogFormScreen(),
+        AppRoutes.reports:   (_) => const ReportsScreen(),
+      },
 
-        routes: {
-          AppRoutes.login:     (_) => LoginScreen(),
-          AppRoutes.dashboard: (_) => DashboardScreen(),
-          AppRoutes.counter:   (_) => CounterScreen(),
-          AppRoutes.logForm:   (_) => LogFormScreen(),
-          AppRoutes.reports:   (_) => ReportsScreen(),
-        },
-
-        // WHY onUnknownRoute:
-        // If somehow the app navigates to a route that doesn't exist,
-        // instead of crashing we send the user back to the login screen.
-        onUnknownRoute: (settings) => MaterialPageRoute(
-          builder: (_) => LoginScreen(),
-        ),
+    
+      onUnknownRoute: (settings) => MaterialPageRoute(
+        builder: (_) => const LoginScreen(),
       ),
     );
   }
 }
+
+
 class AppRoutes {
   AppRoutes._(); 
 
